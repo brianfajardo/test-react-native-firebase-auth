40290fe1dbace4e4aa7aa6ecde36766a739782d1
Brian Fajardo <bmf5@hotmail.com>
2017-05-10 19:21:08 -0400

    Firebase login, handle error cases

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,28 +1,68 @@
 import React, { Component } from 'react'
-import { View } from 'react-native'
+import { View, Text } from 'react-native'
 import { FormLabel, FormInput, Button, Icon } from 'react-native-elements'
+import firebase from 'firebase'
 
 import Input from './Input'
 
 export default class LoginForm extends Component {
   constructor(props) {
     super(props)
-    // Controlled input
-    this.state = { email: '', password: '' }
+    // Controlled inputs
+    this.state = {
+      email: '',
+      password: '',
+      error: ''
+    }
+    this.onButtonPress = this.onButtonPress.bind(this)
+  }
+
+  onButtonPress() {
+    const { email, password } = this.state
+
+    // Clear error on next login attempt
+    this.setState({ error: '' })
+
+    // auth() returns a Promise
+    // If initial sign in fails, attempt to create account
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .catch(() => {
+        console.log('first catch', email, password)
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+          .catch(() => {
+            console.log('second catch', email, password)
+            this.setState({ error: 'Uh oh! Authentication failed.' })
+          })
+      })
+  }
+
+  renderError() {
+    const { error } = this.state
+
+    return (
+      error
+        ? <Text style={style.errorStyle}>{error}</Text>
+        : null
+    )
   }
 
   render() {
-    const { buttonStyle, buttonContainer } = style
+    const { buttonStyle, buttonContainer, errorContainer } = style
 
     return (
       <View style={{ flex: 1, justifyContent: 'space-around' }}>
-        <View style={buttonContainer}>
+        <View>
           <Icon
             name='account-circle'
             color='orange'
             size={150}
           />
         </View>
+
+        <View style={errorContainer}>
+          {this.renderError()}
+        </View>
+
         <View>
           <Input
             label='Email'
@@ -36,6 +76,7 @@ export default class LoginForm extends Component {
             onChangeText={password => this.setState({ password })}
           />
         </View>
+
         <View style={buttonContainer}>
           <Button
             icon={{
@@ -47,6 +88,7 @@ export default class LoginForm extends Component {
             style={buttonStyle}
             backgroundColor='grey'
             borderRadius={20}
+            onPress={this.onButtonPress}
           />
         </View>
       </View>
@@ -59,6 +101,14 @@ const style = {
     width: 150
   },
   buttonContainer: {
+    alignItems: 'center'
+  },
+  errorStyle: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: 'red'
+  },
+  errorContainer: {
     alignItems: 'center'
   }
 }
